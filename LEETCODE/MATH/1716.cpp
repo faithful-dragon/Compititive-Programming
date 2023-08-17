04b815141a7ae53f63279e2ad6e0c7ffb7b735f5
faithful dragon <faithfulldragon3@gmail.com>
2023-08-17 23:20:04 +0530

    1716. Calculate Money in Leetcode Bank

---
+++ LEETCODE/MATH/1716.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    int totalMoney(int n) {
+        int i,j,k,a,b,x=1,y=7,sum=0;
+        a=n/7;
+        b=n%7;
+        for(i=1;i<=a;i++)
+        {
+            for(j=x;j<=y;j++)
+            {
+                sum+=j;
+            }
+            x++;
+            y++;
+        }
+        for(k=1;k<=b;k++)
+        {
+            sum+=x;
+            x++;
+        }
+        return sum;
+    }
+};
