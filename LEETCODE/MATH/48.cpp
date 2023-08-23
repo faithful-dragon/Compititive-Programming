b6712427b4dd48e0a9b7f2f45981a8c4de0e0ec3
faithful dragon <faithfulldragon3@gmail.com>
2023-08-23 11:40:37 +0530

    48. Rotate Image

---
+++ LEETCODE/MATH/48.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(),matrix.end());
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<=i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+    }
+};
