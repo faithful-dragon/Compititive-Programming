d471622b04c71e33fa70a84db936dbb32ea40772
faithful dragon <faithfulldragon3@gmail.com>
2023-08-25 23:37:51 +0530

    fibonaci number using tabulation

--- DYNAMIC_PROGRAMMING/1.cpp
+++ DYNAMIC_PROGRAMMING/1.cpp
@@ -6,10 +6,24 @@ using namespace std;
 // TC = O(N)
 // SC = 2*O(N)
 
+// memorization
 int fib1(int n, vector<int>&dp){
     if(n<=1)return dp[n] = n;
     else if(dp[n]!=-1) return dp[n];
     else return dp[n] = fib1(n-1,dp) + fib1(n-2,dp);
+}
+
+// tabulation
+// TC = O(N)
+// SC = O(N)
+int fib2(int n){
+    vector<int>dp(n+1);
+    dp[0]=0;
+    dp[1]=1;
+    for(int i=2;i<=n;i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[n];
 }
 
 int main(){
@@ -17,6 +31,8 @@ int main(){
     cin>>t;
     vector<int>dp(t+1,-1);
     int nth = fib1(t,dp);
+    cout<<t<<"th fib no : "<<nth<<endl;
+    nth = fib2(t);
     cout<<t<<"th fib no : "<<nth<<endl;
     return 0;
 }
