77abd9691036da01a03d8889d73fe723c86e9cfd
faithful dragon <faithfulldragon3@gmail.com>
2023-08-26 10:05:49 +0530

    possible ways to climb stairs both memorizaton and tabulation method

--- DYNAMIC_PROGRAMMING/2.cpp
+++ DYNAMIC_PROGRAMMING/2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 // memorization
 int possible_ways(int n, vector<int>&dp){
     if(n<0)return 0;
-    else if(n<=2)return dp[n]=1;
+    else if(n<=1)return dp[n]=1;
     else if(dp[n]!=-1)return dp[n];
     else return dp[n]=possible_ways(n-1, dp) + possible_ways(n-2, dp);
 }
@@ -32,7 +32,7 @@ int main(){
     int t;
     cin>>t;
     vector<int>dp(t+2,-1);
-    int x = possible_ways(t+1,dp);
+    int x = possible_ways(t,dp);
     cout<<"no of possoble ways :  "<<x<<endl;
     return 0;
 }
