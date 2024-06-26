0cd5f824b224edcebdb5ed5986898173c7ec96c0
faithful dragon <faithfulldragon3@gmail.com>
2024-06-26 21:44:53 +0530

    linear search

--- 4.SEARCH_ALGO/1.cpp
+++ 4.SEARCH_ALGO/1.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 using namespace std;
 
+// total comparision : 2N
 int linear_search(int arr[], int n, int key){
-    for(int i=0;i<n;i++){
-        if(arr[i]==key)return i;
+    for(int i=0;i<n;i++){    // no of comparision : n
+        if(arr[i]==key)return i;   // no of comparision : n
     }
     return -1;
 }
