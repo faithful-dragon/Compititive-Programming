3bf81cfdb36b4a24a5144a7b01210c83e3d16c55
faithful dragon <faithfulldragon3@gmail.com>
2024-06-26 21:40:12 +0530

    linear search

---
+++ 4.SEARCH_ALGO/1.cpp
@@ -0,0 +1,24 @@
+// linear search
+#include <iostream>
+using namespace std;
+
+int linear_search(int arr[], int n, int key){
+    for(int i=0;i<n;i++){
+        if(arr[i]==key)return i;
+    }
+    return -1;
+}
+
+int main(){
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+    int idx;
+    int key = 9;
+    idx = linear_search(a,10,key);
+    if(idx == -1)cout<<"Not found"<<endl;
+    else cout<<"Found at index "<<idx<<endl;
+
+    key=0;
+    idx = linear_search(a,10,key);
+    if(idx == -1)cout<<"Not found"<<endl;
+    else cout<<"Found at index "<<idx<<endl;
+}
