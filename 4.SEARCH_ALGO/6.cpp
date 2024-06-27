26159630c4af873e5e7da789933f1e92e8562d29
faithful dragon <faithfulldragon3@gmail.com>
2024-06-27 18:28:56 +0530

    floor and ceil value

---
+++ 4.SEARCH_ALGO/6.cpp
@@ -0,0 +1,60 @@
+// first and last occurence
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int floor(vector<int> &arr, int n, int key){   // largest <= key
+    int low = 0;
+    int high = n-1;
+    int mid;
+    int ans = n;
+    
+    while(low<=high){
+        mid = low + (high-low)/2;         // to overcome overflow
+        if(arr[mid]<=key){
+            low=mid+1;
+            ans=mid;
+        }
+        else high=mid-1;
+    }
+    return ans;
+}
+    
+    // total comparision : O(log2(N))
+int ceil(vector<int> &arr, int n, int key){   // smallest >=key
+    int low = 0;
+    int high = n-1;
+    int mid;
+    int ans = n;
+
+    while(low<=high){
+        mid = low + (high-low)/2;         // to overcome overflow
+        if(arr[mid]>=key){
+            high=mid-1;
+            ans=mid;
+        }
+        else low=mid+1;
+    }
+    return ans;
+}
+
+int main(){
+    const int n = 13;
+    vector<int>v{1,3,4,4,4,4,5,6,7,8,9,20};
+    int x = 10;
+    int low = floor(v,n,x);
+    int high = ceil(v,n,x);
+    if(low==n)cout<<"not found"<<endl;
+    else cout<<"floor value : "<<v[low]<<endl;
+    if(high==n)cout<<"not found"<<endl;
+    else cout<<"ceil value : "<<v[high]<<endl;
+
+    x = 2;
+    low = floor(v,n,x);
+    high = ceil(v,n,x);
+    if(low==n)cout<<"not found"<<endl;
+    else cout<<"floor value : "<<v[low]<<endl;
+    if(high==n)cout<<"not found"<<endl;
+    else cout<<"ceil value : "<<v[high]<<endl;
+
+}
